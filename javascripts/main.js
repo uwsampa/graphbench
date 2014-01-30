88ee1d86b96a43eb3555054ea6f37651dee21b8f
emetchar <emetchar@gmail.com>
2014-01-29 21:20:01 -0800

    test page separation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+angular.module('gbApp', []).
+    config(function ($routeProvider) {
+        $routeProvider.when('/:primaryNav', {
+            templateUrl: 'index.html',
+            controller: 'RouteController'
+        });
+    });
+
+function RouteController($scope, $routeParams) {
+		if ($routeParams.primaryNav) {
+			$scope.page = $routeParams.primaryNav;
+		} else {
+			$scope.page = 'intro';
+		}
+		
+    }
