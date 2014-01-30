d482e8d1e9a710e27b445d6c64fdcf8a168d6bc0
emetchar <emetchar@gmail.com>
2014-01-29 22:07:49 -0800

    another test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,27 @@
-angular.module('gbApp', []).
-    config(function ($routeProvider) {
-        $routeProvider.when('/:primaryNav', {
-            templateUrl: 'index.html',
-            controller: 'RouteController'
-        });
-    });
+var app = angular.module('gbApp', ["ngRoute"]);
 
-function RouteController($scope, $routeParams) {
-		if ($routeParams.primaryNav) {
-			$scope.page = $routeParams.primaryNav;
-		} else {
-			$scope.page = 'intro';
-		}
-		
+app.controller('MainCntl', function($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+});
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/intro',
+    {
+      templateUrl: "contents/intro.html",
+      controller: "RouteCntl"
     }
+  )
+  $routeProvider.otherwise({redirectTo: '/intro'});
+});
+
+function MainCntl($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+}
+
+function RouteCntl($scope, $routeParams) {
+}
