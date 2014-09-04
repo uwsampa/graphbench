b57e0aefbbc44582d4389007f5908ac3bf030c83
Cindyxinxin <yxcynthia@gmail.com>
2014-09-04 15:57:43 -0700

    Create JsonDoubleDoubleFloatDoubleVertexInputFormat.java

---
+++ Giraph/TriangleCounting/JsonDoubleDoubleFloatDoubleVertexInputFormat.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.giraph.io.formats;
+
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.edge.EdgeFactory;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+  * VertexInputFormat that features <code>double</code> vertex ID's,
+  * <code>double</code> vertex values and <code>float</code>
+  * out-edge weights, and <code>double</code> message types,
+  *  specified in JSON format.
+  */
+public class JsonDoubleDoubleFloatDoubleVertexInputFormat extends
+  TextVertexInputFormat<DoubleWritable, DoubleWritable, FloatWritable> {
+
+  @Override
+  public TextVertexReader createVertexReader(InputSplit split,
+      TaskAttemptContext context) {
+    return new JsonDoubleDoubleFloatDoubleVertexReader();
+  }
+
+ /**
+  * VertexReader that features <code>double</code> vertex
+  * values and <code>float</code> out-edge weights. The
+  * files should be in the following JSON format:
+  * JSONArray(<vertex id>, <vertex value>,
+  *   JSONArray(JSONArray(<dest vertex id>, <edge value>), ...))
+  * Here is an example with vertex id 1, vertex value 4.3, and two edges.
+  * First edge has a destination vertex 2, edge value 2.1.
+  * Second edge has a destination vertex 3, edge value 0.7.
+  * [1,4.3,[[2,2.1],[3,0.7]]]
+  */
+  class JsonDoubleDoubleFloatDoubleVertexReader extends
+    TextVertexReaderFromEachLineProcessedHandlingExceptions<JSONArray,
+    JSONException> {
+
+    @Override
+    protected JSONArray preprocessLine(Text line) throws JSONException {
+      return new JSONArray(line.toString());
+    }
+
+    @Override
+    protected DoubleWritable getId(JSONArray jsonVertex) throws JSONException,
+              IOException {
+      return new DoubleWritable(jsonVertex.getDouble(0));
+    }
+
+    @Override
+    protected DoubleWritable getValue(JSONArray jsonVertex) throws
+      JSONException, IOException {
+      return new DoubleWritable(jsonVertex.getDouble(1));
+    }
+
+    @Override
+    protected Iterable<Edge<DoubleWritable, FloatWritable>> getEdges(
+        JSONArray jsonVertex) throws JSONException, IOException {
+      JSONArray jsonEdgeArray = jsonVertex.getJSONArray(2);
+      List<Edge<DoubleWritable, FloatWritable>> edges =
+          Lists.newArrayListWithCapacity(jsonEdgeArray.length());
+      for (int i = 0; i < jsonEdgeArray.length(); ++i) {
+        JSONArray jsonEdge = jsonEdgeArray.getJSONArray(i);
+        edges.add(EdgeFactory.create(new DoubleWritable(jsonEdge.getDouble(0)),
+            new FloatWritable((float) jsonEdge.getDouble(1))));
+      }
+      return edges;
+    }
+
+    @Override
+    protected Vertex<DoubleWritable, DoubleWritable, FloatWritable>
+    handleException(Text line, JSONArray jsonVertex, JSONException e) {
+      throw new IllegalArgumentException(
+          "Couldn't get vertex from line " + line, e);
+    }
+
+  }
+}
