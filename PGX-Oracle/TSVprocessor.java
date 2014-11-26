a2a717fb55781f85659dda2d5108644ddbb8caf7
Cindy Xin Yi <yxcynthia@gmail.com>
2014-11-26 12:41:21 -0800

    Create TSVprocessor.java

---
+++ PGX-Oracle/TSVprocessor.java
@@ -0,0 +1,31 @@
+package processor;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+public class TSVprocessor {
+	public static void main(String args[]) {
+		try {
+			File file = new File("output.txt");
+			file.createNewFile();
+			FileWriter writer = new FileWriter(file);
+			FileInputStream fstream = new FileInputStream("facebook_combined.txt");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine = null;
+			while ((strLine = br.readLine()) != null) {
+				String[] token = strLine.split(" ");
+				writer.write(token[0] + "\t" + token[1] + "\n");
+			}
+			in.close();
+			writer.flush();
+			writer.close();
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+}
