0a219562bc93ad3b1449a8dbf0951e7e085136b2
Cindy Xin Yi <yxcynthia@gmail.com>
2014-11-30 16:21:14 -0800

    Update TSVprocessor.java
    enable comments start with # as comments in the original file

--- PGX-Oracle/TSVprocessor.java
+++ PGX-Oracle/TSVprocessor.java
@@ -7,6 +7,19 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 
+/*
+This class accept the file in the following format:
+eg: a b (split by space)
+    c d 
+    e f
+    g h
+The goal is to change the file into the tab separated format:
+eg: a	b (split by tab)
+    c	d
+    e	f
+    g	h
+It will ignore any line that starts with #
+*/
 public class TSVprocessor {
 	public static void main(String args[]) {
 		try {
@@ -18,8 +31,10 @@ public class TSVprocessor {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String strLine = null;
 			while ((strLine = br.readLine()) != null) {
-				String[] token = strLine.split(" ");
-				writer.write(token[0] + "\t" + token[1] + "\n");
+				if (!strLine.startsWith("#")) {
+					String[] token = strLine.split(" ");
+					writer.write(token[0] + "\t" + token[1] + "\n");
+				}
 			}
 			in.close();
 			writer.flush();
