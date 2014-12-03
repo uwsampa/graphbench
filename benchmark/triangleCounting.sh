52f04cb26218b00339264a4a3eb5ee45788bf0ee
Cindy Xin Yi <yxcynthia@gmail.com>
2014-12-03 13:17:19 -0800

    Update triangleCounting.sh

--- benchmark/triangleCounting.sh
+++ benchmark/triangleCounting.sh
@@ -4,6 +4,7 @@ if [ "$#" -eq  "1" ]
 mkdir triangle
 javac -cp $PGX_HOME/lib/*:$PGX_HOME/third-party/* triangleCounting.java -d triangle/
 java -Dlog4j.configuration=file:$PGX_HOME/conf/log4j.xml -cp $PGX_HOME/lib/*:$PGX_HOME/third-party/*:triangle triangleCounting $1
+rm -r triangle
 else
 echo "usage: triangleCounting <graph config path>"
 fi
