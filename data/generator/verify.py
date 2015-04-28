114bf65ae5ba7eeaabe164acfceacfdfcdb57a80
Xin(Cindy) Yi <yxcynthia@gmail.com>
2015-04-28 11:26:27 -0700

    verify

--- data/generator/verify.py
+++ data/generator/verify.py
@@ -2,14 +2,17 @@
 from matplotlib import pyplot as PLT
 import numpy as NP
 
-with open('out10.txt') as f:
-  v = NP.loadtxt(f, delimiter="	", dtype='float', usecols=None)
+with open('out14.txt') as f:
+  v = NP.loadtxt(f, dtype='int', usecols=[0, 1])
 
 from matplotlib import pyplot as PLT
 
+# v_hist = v;
 v_hist = NP.ravel(v)   # 'flatten' v
 fig = PLT.figure()
 ax1 = fig.add_subplot(111)
 
-n, bins, patches = ax1.hist(v_hist, normed=1, bins=1000, facecolor='green')
+n, bins, patches = ax1.hist(v_hist, bins=NP.logspace(1, 5, 100), facecolor='green', log=True)
+ax1.set_xscale('log')
+ax1.invert_xaxis()
 PLT.show()
