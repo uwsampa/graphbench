a64d4c488c9bd62374d8e72bc03fb21a0b1d673e
Jacob Nelson <nelson@cs.washington.edu>
2015-04-28 12:30:44 -0700

    actually plot *degree* distribution!

--- data/generator/verify.py
+++ data/generator/verify.py
@@ -1,18 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 from matplotlib import pyplot as PLT
 import numpy as NP
+import collections
+import sys
 
-with open('out14.txt') as f:
-  v = NP.loadtxt(f, dtype='int', usecols=[0, 1])
+if len(sys.argv) != 2:
+  print "Usage: verify.py <filename>"
+  exit(1)
 
-from matplotlib import pyplot as PLT
+with open(sys.argv[1]) as f:
+  v = NP.loadtxt(f, dtype='int', usecols=[1])
 
-# v_hist = v;
 v_hist = NP.ravel(v)   # 'flatten' v
+
+vertex_degrees = collections.Counter(v_hist)
+degrees = collections.Counter(vertex_degrees.values())
+
 fig = PLT.figure()
 ax1 = fig.add_subplot(111)
 
-n, bins, patches = ax1.hist(v_hist, bins=NP.logspace(1, 5, 100), facecolor='green', log=True)
+ax1.scatter(degrees.keys(), degrees.values())
+
 ax1.set_xscale('log')
-ax1.invert_xaxis()
+ax1.set_yscale('log')
+
 PLT.show()
