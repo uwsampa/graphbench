b01593ead459a8e53c135dab4e9ef6af84224c00
Cindy Xin Yi <yxcynthia@gmail.com>
2015-05-26 11:52:28 -0700

    Update SSSP.sh

--- benchmark/pgx-oracle/sssp/SSSP.sh
+++ benchmark/pgx-oracle/sssp/SSSP.sh
@@ -2,9 +2,9 @@
 if [ "$#" -eq  "4" ] 
 	then
 mkdir sssp
-javac -cp $PGX_HOME/lib/*:$PGX_HOME/third-party/* SSSP.java -d sssp/
-java -Dlog4j.configuration=file:$PGX_HOME/conf/log4j.xml -cp $PGX_HOME/lib/*:$PGX_HOME/third-party/*:sssp SSSP $1 $2 $3 $4
+javac -cp $PGX_HOME/lib/common/*:$PGX_HOME/lib/yarn/*:$PGX_HOME/lib/embedded/*:$PGX_HOME/lib/client/*:$PGX_HOME/third-party/* SSSP.java -d sssp/
+java -Dlog4j.configuration=file:$PGX_HOME/conf/log4j.xml -cp $PGX_HOME/lib/common/*:$PGX_HOME/lib/yarn/*:$PGX_HOME/lib/embedded/*:$PGX_HOME/lib/client/*:$PGX_HOME/third-party/*:sssp SSSP $1 $2 $3 $4
 rm -r sssp
 else
-echo "usage: pagerank <graph config path> <costPropName in the .json file> <src node name> <dest node name>"
+echo "usage: ./SSSP.sh <graph config path> <costPropName in the .json file> <src node name> <dest node name>"
 fi
